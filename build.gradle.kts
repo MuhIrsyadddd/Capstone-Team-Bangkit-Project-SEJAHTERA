6fab1b93bfdb426b039adcd7290384c4bcf55cb3
MuhIrsyad <83459106+MuhIrsyadddd@users.noreply.github.com>
2024-12-16 18:33:00 +0700

    Add files via upload
    benerin

--- build.gradle.kts
+++ build.gradle.kts
@@ -1,18 +1,73 @@
-// Top-level build file where you can add configuration options common to all sub-projects/modules.
+plugins {
+    alias(libs.plugins.android.application)
+    alias(libs.plugins.jetbrains.kotlin.android)
+    id("com.google.gms.google-services")
+    id("kotlin-kapt") // Tambahkan ini
+}
 
-buildscript {
-    repositories {
-        google()
-        mavenCentral() // Pastikan baris ini ada
-        maven("https://jitpack.io") // Jika menggunakan pustaka dari JitPack
+android {
+    namespace = "com.example.capstonesejahtera"
+    compileSdk = 35
+
+    defaultConfig {
+        applicationId = "com.example.capstonesejahtera"
+        minSdk = 24
+        targetSdk = 35
+        versionCode = 1
+        versionName = "1.0"
+
+        testInstrumentationRunner = "androidx.test.runner.AndroidJUnitRunner"
     }
-    dependencies {
-        classpath("com.google.gms:google-services:4.4.2")
+
+    buildTypes {
+        release {
+            isMinifyEnabled = false
+            proguardFiles(
+                getDefaultProguardFile("proguard-android-optimize.txt"),
+                "proguard-rules.pro"
+            )
+        }
+    }
+
+    buildFeatures {
+        viewBinding = true
+    }
+
+    compileOptions {
+        sourceCompatibility = JavaVersion.VERSION_1_8
+        targetCompatibility = JavaVersion.VERSION_1_8
+    }
+
+    kotlinOptions {
+        jvmTarget = "1.8"
     }
 }
 
-plugins {
-    alias(libs.plugins.android.application) apply false
-    alias(libs.plugins.jetbrains.kotlin.android) apply false
-    alias(libs.plugins.google.gms.google.services) apply false
+dependencies {
+    implementation(libs.androidx.core.ktx)
+    implementation(libs.androidx.appcompat)
+    implementation(libs.material)
+    implementation(libs.androidx.activity)
+    implementation(libs.androidx.constraintlayout)
+    implementation(libs.firebase.firestore)
+    implementation(libs.firebase.auth.ktx)
+    testImplementation(libs.junit)
+    androidTestImplementation(libs.androidx.junit)
+    androidTestImplementation(libs.androidx.espresso.core)
+
+    implementation("com.google.firebase:firebase-firestore:25.1.0")
+    implementation("com.google.android.gms:play-services-auth:20.7.0")
+
+    implementation("com.squareup.retrofit2:retrofit:2.9.0")
+    implementation("com.squareup.retrofit2:converter-gson:2.9.0")
+    implementation("androidx.recyclerview:recyclerview:1.2.1")
+
+    // Tambahkan dependensi Glide
+    implementation("com.github.bumptech.glide:glide:4.14.2")
+    kapt("com.github.bumptech.glide:compiler:4.14.2")
+
+    implementation ("com.google.firebase:firebase-database:20.2.2")
+
+    implementation ("com.github.PhilJay:MPAndroidChart:v3.1.0")
+
 }
