5813f61bcabecfaba1bedc305dca00fa19190df1
MuhIrsyad <83459106+MuhIrsyadddd@users.noreply.github.com>
2024-12-16 18:31:55 +0700

    Delete app directory
    buat benerin

--- app/src/main/java/com/example/capstonesejahtera/StockItem.kt
+++
@@ -1,3 +0,0 @@
-package com.example.capstonesejahtera
-
-data class StockItem(val name: String)
--- app/src/main/java/com/example/capstonesejahtera/SummaryItem.kt
+++
@@ -1,6 +0,0 @@
-package com.example.capstonesejahtera
-
-data class SummaryItem(
-    val title: String,
-    val totalNominal: Long
-)
